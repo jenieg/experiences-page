a3746148d22a54a7447f60e895c6ba3532dc39a1
Jennifer <jmarthagodfrey@gmail.com>
2024-09-16 11:56:27 -0600

    Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,13 @@ import './hero.css'
 
 const Hero = () => {
     return (
-        <main className="hero">
+        <section className="hero">
             <img src={hero} alt="Gallery of Experiences" className="hero--img"/>
             <div className="hero--text">
                 <h1>Online Experiences</h1>
                 <p>Join unique interactive activities led by one-of-a-kind hosts; all without leaving home.</p>
             </div>
-        </main>
+        </section>
     );
 };
 
